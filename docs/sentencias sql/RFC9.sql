62c867f397d322d57e8f41c973a498c6d6c13ddb
khcastrillon <47218395+khcastrillon@users.noreply.github.com>
2020-05-25 19:05:08 -0500

    Update RFC9.sql

--- docs/sentencias sql/RFC9.sql
+++ docs/sentencias sql/RFC9.sql
@@ -1,10 +1,11 @@
-/*
-RFC9- ENCONTRAR LAS OFERTAS DE ALOJAMIENTO QUE NO TIENEN MUCHA DEMANDA
+--RFC9 - ENCONTRAR LAS OFERTAS DE ALOJAMIENTO QUE NO TIENEN MUCHA DEMANDA--
 
-Encontrar las ofertas de alojamiento que no han recibido clientes en periodos superiores a 1 mes, durante todo el periodo de operaciÃ³n de AlohAndes.
-*/
+/*Encontrar las ofertas de alojamiento que no han recibido clientes en periodos superiores a 1 mes, durante todo
+el periodo de operación de AlohAndes.*/
 
-SELECT *
-FROM ALOJAMIENTOS a, RESERVAS r
-WHERE a.id = r.idalojamiento
-AND r.tiempodias <30;
+SELECT id
+FROM (SELECT a.id, sum(r.tiempodias) cantidad
+        FROM RESERVAS r, ALOJAMIENTOS a
+        WHERE r.idalojamiento = a.id
+        group by a.id)
+WHERE cantidad < 30;
