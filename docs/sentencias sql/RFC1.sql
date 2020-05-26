84c39f2f94c1a4974ff3f61d1bafab7601f25385
khcastrillon <47218395+khcastrillon@users.noreply.github.com>
2020-05-26 14:29:10 -0500

    Update RFC1.sql

--- docs/sentencias sql/RFC1.sql
+++ docs/sentencias sql/RFC1.sql
@@ -1,21 +1,19 @@
 --RFC1 - MOSTRAR EL DINERO RECIBIDO POR CADA PROVEEDOR DE ALOJAMIENTO DURANTE EL AÑO ACTUAL Y EL AÑO CORRIDO
 
-SELECT idpe, nom, dinero_Recibido_Año_Actual, dinero_Recibido_Añocorrido
+SELECT idpe, nom, dinero_Recibido_Anio_Actual, dinero_Recibido_Aniocorrido
 FROM
-    (SELECT op.id AS idpe, op.nombre AS nom , sum(alo.precio * re.tiempodias) AS dinero_Recibido_Año_Actual
+    (SELECT op.id AS idpe, op.nombre AS nom , sum(alo.precio * re.tiempodias) AS dinero_Recibido_Anio_Actual
      FROM  ALOJAMIENTOS alo,  RESERVAS re, OPERADORES op
            WHERE op.id = alo.idoperador
            AND alo.id = re.idalojamiento
            AND re.diareserva BETWEEN '01/01/20' AND '31/12/20'
-     GROUP BY op.id, op.nombre
-     ORDER BY dinero_Recibido_Año_Actual DESC),
-
-    (SELECT op.id, op.nombre , sum(alo.precio * re.tiempodias) AS dinero_Recibido_Añocorrido
+     GROUP BY op.id, op.nombre) INNER JOIN
+    (SELECT op.id idpe1, op.nombre nom1, sum(alo.precio * re.tiempodias) AS dinero_Recibido_Aniocorrido
      FROM  ALOJAMIENTOS alo,  RESERVAS re, OPERADORES op
            WHERE op.id = alo.idoperador
            AND alo.id = re.idalojamiento
            AND re.diareserva BETWEEN '01/01/20' AND '26/05/20'
-     GROUP BY op.id, op.nombre
-     ORDER BY dinero_Recibido_Añocorrido DESC) ;
+     GROUP BY op.id, op.nombre)
+ON idpe = idpe1 AND nom = nom1;
 
 
